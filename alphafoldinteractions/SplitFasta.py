e3f4c29ada67b0c97c730c34b2f405065eb0a431
Christian Poitras <christian.poitras@ircm.qc.ca>
2023-03-21 19:03:31 -0400

    Fixes indentation of SplitFasta.py

--- alphafoldinteractions/SplitFasta.py
+++ alphafoldinteractions/SplitFasta.py
@@ -16,23 +16,23 @@ def main():
     args = parser.parse_args()
 
     if args.outdir and not os.path.exists(args.outdir):
-      os.makedirs(args.outdir)
+        os.makedirs(args.outdir)
 
     lines = args.infile.readlines()
     for line_index in range(0, len(lines)):
-      line = lines[line_index]
-      line_index = line_index + 1
-      if line.startswith('>'):
-        name = re.search(r"^>(\w+)", line)[1]
-        if not name:
-          print(f"{name} cannot be empty", file=sys.stderr)
-          continue
-        with open(os.path.join(args.outdir, name+".fasta"), 'w') as outfile:
-          outfile.write(line)
-          while line_index < len(lines) and not lines[line_index].startswith('>'):
-            line = lines[line_index]
-            line_index = line_index + 1
-            outfile.write(line)
+        line = lines[line_index]
+        line_index = line_index + 1
+        if line.startswith('>'):
+            name = re.search(r"^>(\w+)", line)[1]
+            if not name:
+                print(f"{name} cannot be empty", file=sys.stderr)
+                continue
+            with open(os.path.join(args.outdir, name+".fasta"), 'w') as outfile:
+                outfile.write(line)
+                while line_index < len(lines) and not lines[line_index].startswith('>'):
+                    line = lines[line_index]
+                    line_index = line_index + 1
+                    outfile.write(line)
 
 
 if __name__ == '__main__':

4b2364d64bd62d054e74dbb2395781f210a79b4e
Christian Poitras <christian.poitras@ircm.qc.ca>
2023-03-21 18:46:23 -0400

    Adds setup.py and fixes CountInteractions.py to use main function.

--- alphafoldinteractions/CountInteractions.py
+++ alphafoldinteractions/CountInteractions.py
@@ -14,74 +14,79 @@ def potential_interactor_atoms(chain):
     return atoms
 
 
-parser = argparse.ArgumentParser(description="Counts the number of potentials interactions between " +
-                                             "residues of two proteins.")
-parser.add_argument('infile', nargs='?', type=argparse.FileType('r'), default=sys.stdin,
-                    help="PDB file created by AlphaFold containing two proteins of interest")
-parser.add_argument('-n', '--name',
-                    help="Name of structure in PDB file (default: PDB filename or 'Unknown' if standard input)")
-parser.add_argument('-r', '--radius', type=float, default=6.0,
-                    help="Distance between atoms from different residues to assume interaction (default: %(default)s)")
-parser.add_argument('-R', '--residues', type=argparse.FileType('w'), metavar="RESIDUES",
-                    help="Save pairs of residues in tab separated file %(metavar)s")
-parser.add_argument('-A', '--atoms', type=argparse.FileType('w'), metavar="ATOMS",
-                    help="Save pairs of atoms in tab separated file %(metavar)s")
+def main():
+    parser = argparse.ArgumentParser(description="Counts the number of potentials interactions between " +
+                                                 "residues of two proteins.")
+    parser.add_argument('infile', nargs='?', type=argparse.FileType('r'), default=sys.stdin,
+                        help="PDB file created by AlphaFold containing two proteins of interest")
+    parser.add_argument('-n', '--name',
+                        help="Name of structure in PDB file (default: PDB filename or 'Unknown' if standard input)")
+    parser.add_argument('-r', '--radius', type=float, default=6.0,
+                        help="Distance between atoms from different residues to assume interaction (default: %(default)s)")
+    parser.add_argument('-R', '--residues', type=argparse.FileType('w'), metavar="RESIDUES",
+                        help="Save pairs of residues in tab separated file %(metavar)s")
+    parser.add_argument('-A', '--atoms', type=argparse.FileType('w'), metavar="ATOMS",
+                        help="Save pairs of atoms in tab separated file %(metavar)s")
 
-args = parser.parse_args()
-if not args.name:
-    if args.infile == sys.stdin:
-        args.name = "Unknown"
-    else:
-        args.name = os.path.basename(os.path.splitext(args.infile.name)[0])
+    args = parser.parse_args()
+    if not args.name:
+        if args.infile == sys.stdin:
+            args.name = "Unknown"
+        else:
+            args.name = os.path.basename(os.path.splitext(args.infile.name)[0])
 
-parser = PDBParser()
-structure = parser.get_structure(args.name, args.infile)
+    parser = PDBParser()
+    structure = parser.get_structure(args.name, args.infile)
 
-atoms_a = potential_interactor_atoms(structure[0]["A"])
-atoms_b = potential_interactor_atoms(structure[0]["B"])
+    atoms_a = potential_interactor_atoms(structure[0]["A"])
+    atoms_b = potential_interactor_atoms(structure[0]["B"])
 
-interactions_search = NeighborSearch(atoms_a + atoms_b)
-interactions = interactions_search.search_all(args.radius, level='R')
-interactions = [residue_pair for residue_pair in interactions if residue_pair[0].get_parent() != residue_pair[1].get_parent()]
+    interactions_search = NeighborSearch(atoms_a + atoms_b)
+    interactions = interactions_search.search_all(args.radius, level='R')
+    interactions = [residue_pair for residue_pair in interactions if residue_pair[0].get_parent() != residue_pair[1].get_parent()]
 
-print(f"{len(interactions)}")
+    print(f"{len(interactions)}")
 
-if args.residues:
-    args.residues.write("Chain A\tResidue number A\tResidue name A\tChain B\tResidue number B\tResidue name B\n")
-    for residue_pair in interactions:
-        args.residues.write(f"{residue_pair[0].get_parent().get_id()}")
-        args.residues.write("\t")
-        args.residues.write(f"{residue_pair[0].get_id()[1]}")
-        args.residues.write("\t")
-        args.residues.write(f"{residue_pair[0].get_resname()}")
-        args.residues.write("\t")
-        args.residues.write(f"{residue_pair[1].get_parent().get_id()}")
-        args.residues.write("\t")
-        args.residues.write(f"{residue_pair[1].get_id()[1]}")
-        args.residues.write("\t")
-        args.residues.write(f"{residue_pair[1].get_resname()}")
-        args.residues.write("\n")
+    if args.residues:
+        args.residues.write("Chain A\tResidue number A\tResidue name A\tChain B\tResidue number B\tResidue name B\n")
+        for residue_pair in interactions:
+            args.residues.write(f"{residue_pair[0].get_parent().get_id()}")
+            args.residues.write("\t")
+            args.residues.write(f"{residue_pair[0].get_id()[1]}")
+            args.residues.write("\t")
+            args.residues.write(f"{residue_pair[0].get_resname()}")
+            args.residues.write("\t")
+            args.residues.write(f"{residue_pair[1].get_parent().get_id()}")
+            args.residues.write("\t")
+            args.residues.write(f"{residue_pair[1].get_id()[1]}")
+            args.residues.write("\t")
+            args.residues.write(f"{residue_pair[1].get_resname()}")
+            args.residues.write("\n")
 
-if args.atoms:
-    interactions = interactions_search.search_all(args.radius, level='A')
-    interactions = [atom_pair for atom_pair in interactions
-                    if atom_pair[0].get_parent().get_parent() != atom_pair[1].get_parent().get_parent()]
-    args.atoms.write("Chain A\tResidue number A\tResidue name A\tAtom A\t" +
-                     "Chain B\tResidue number B\tResidue name B\tAtom B\n")
-    for atom_pair in interactions:
-        args.atoms.write(f"{atom_pair[0].get_parent().get_parent().get_id()}")
-        args.atoms.write("\t")
-        args.atoms.write(f"{atom_pair[0].get_parent().get_id()[1]}")
-        args.atoms.write("\t")
-        args.atoms.write(f"{atom_pair[0].get_parent().get_resname()}")
-        args.atoms.write("\t")
-        args.atoms.write(f"{atom_pair[0].get_name()}")
-        args.atoms.write("\t")
-        args.atoms.write(f"{atom_pair[1].get_parent().get_parent().get_id()}")
-        args.atoms.write("\t")
-        args.atoms.write(f"{atom_pair[1].get_parent().get_id()[1]}")
-        args.atoms.write("\t")
-        args.atoms.write(f"{atom_pair[1].get_parent().get_resname()}")
-        args.atoms.write("\t")
-        args.atoms.write(f"{atom_pair[1].get_name()}")
-        args.atoms.write("\n")
+    if args.atoms:
+        interactions = interactions_search.search_all(args.radius, level='A')
+        interactions = [atom_pair for atom_pair in interactions
+                        if atom_pair[0].get_parent().get_parent() != atom_pair[1].get_parent().get_parent()]
+        args.atoms.write("Chain A\tResidue number A\tResidue name A\tAtom A\t" +
+                         "Chain B\tResidue number B\tResidue name B\tAtom B\n")
+        for atom_pair in interactions:
+            args.atoms.write(f"{atom_pair[0].get_parent().get_parent().get_id()}")
+            args.atoms.write("\t")
+            args.atoms.write(f"{atom_pair[0].get_parent().get_id()[1]}")
+            args.atoms.write("\t")
+            args.atoms.write(f"{atom_pair[0].get_parent().get_resname()}")
+            args.atoms.write("\t")
+            args.atoms.write(f"{atom_pair[0].get_name()}")
+            args.atoms.write("\t")
+            args.atoms.write(f"{atom_pair[1].get_parent().get_parent().get_id()}")
+            args.atoms.write("\t")
+            args.atoms.write(f"{atom_pair[1].get_parent().get_id()[1]}")
+            args.atoms.write("\t")
+            args.atoms.write(f"{atom_pair[1].get_parent().get_resname()}")
+            args.atoms.write("\t")
+            args.atoms.write(f"{atom_pair[1].get_name()}")
+            args.atoms.write("\n")
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="AlphaFoldInteractions",
+    version="1.0-SNAPSHOT",
+    packages=find_packages(),
+    author="Christian Poitras",
+    author_email="christian.poitras@ircm.qc.ca",
+    description="Find interactions using AlphaFold",
+    keywords="bioinformatics, AlphaFold",
+    url="https://github.com/benoitcoulombelab/alphafold-interactions.git",
+    license="GNU General Public License version 3",
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License version 3"
+    ],
+    install_requires=[
+        "biopython>=1.81"
+    ],
+    entry_points={
+        "console_scripts": [
+            "count-interactions = alphafoldinteractions.CountInteractions:main"
+        ]
+    }
+)

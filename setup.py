a36e0cbae094a3a45ec47006f2011e73fb9dd4d0
Christian Poitras <christian.poitras@ircm.qc.ca>
2023-04-15 17:35:38 -0400

    Changes version to 0.1 to allow installation with pip.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="DeepProteinConnector",
-    version="1.0-SNAPSHOT",
+    version="0.1",
     packages=find_packages(),
     author="Christian Poitras",
     author_email="christian.poitras@ircm.qc.ca",

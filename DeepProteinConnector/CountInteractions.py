fcbe4bac3b82ee78dc45b2e1cbe43a2b40d48b99
Christian Poitras <christian.poitras@ircm.qc.ca>
2023-04-27 11:25:26 -0400

    Split CountInteractions.py in multiple functions.

--- DeepProteinConnector/CountInteractions.py
+++ DeepProteinConnector/CountInteractions.py
@@ -5,15 +5,6 @@ import os
 from Bio.PDB import PDBParser, NeighborSearch
 
 
-def potential_interactor_atoms(chain):
-    atoms = []
-    for residue in chain:
-        for atom in residue:
-            if atom.get_name() not in ["N", "CA", "C", "O", "OXT"] and not atom.get_name().startswith("H"):
-                atoms.append(atom)
-    return atoms
-
-
 def main():
     parser = argparse.ArgumentParser(description="Counts the number of potentials interactions between "
                                                  "residues of two proteins.")
@@ -44,51 +35,80 @@ def main():
     atoms_a = potential_interactor_atoms(structure[0]["A"])
     atoms_b = potential_interactor_atoms(structure[0]["B"])
 
-    interactions_search = NeighborSearch(atoms_a + atoms_b)
-    interactions = interactions_search.search_all(args.radius, level='R')
-    interactions = [residue_pair for residue_pair in interactions if residue_pair[0].get_parent() != residue_pair[1].get_parent()]
+    neighbor_search = NeighborSearch(atoms_a + atoms_b)
+    interactions = search_interactions(neighbor_search, args.radius, level='R')
 
-    args.output.write(f"{len(interactions)}")
+    args.output.write(f"{len(interactions)}\n")
 
     if args.residues:
-        args.residues.write("Chain A\tResidue number A\tResidue name A\tChain B\tResidue number B\tResidue name B\n")
-        for residue_pair in interactions:
-            args.residues.write(f"{residue_pair[0].get_parent().get_id()}")
-            args.residues.write("\t")
-            args.residues.write(f"{residue_pair[0].get_id()[1]}")
-            args.residues.write("\t")
-            args.residues.write(f"{residue_pair[0].get_resname()}")
-            args.residues.write("\t")
-            args.residues.write(f"{residue_pair[1].get_parent().get_id()}")
-            args.residues.write("\t")
-            args.residues.write(f"{residue_pair[1].get_id()[1]}")
-            args.residues.write("\t")
-            args.residues.write(f"{residue_pair[1].get_resname()}")
-            args.residues.write("\n")
+        write_residues(interactions, args.radius, args.residues)
 
     if args.atoms:
-        interactions = interactions_search.search_all(args.radius, level='A')
-        interactions = [atom_pair for atom_pair in interactions
-                        if atom_pair[0].get_parent().get_parent() != atom_pair[1].get_parent().get_parent()]
-        args.atoms.write("Chain A\tResidue number A\tResidue name A\tAtom A\t" +
-                         "Chain B\tResidue number B\tResidue name B\tAtom B\n")
-        for atom_pair in interactions:
-            args.atoms.write(f"{atom_pair[0].get_parent().get_parent().get_id()}")
-            args.atoms.write("\t")
-            args.atoms.write(f"{atom_pair[0].get_parent().get_id()[1]}")
-            args.atoms.write("\t")
-            args.atoms.write(f"{atom_pair[0].get_parent().get_resname()}")
-            args.atoms.write("\t")
-            args.atoms.write(f"{atom_pair[0].get_name()}")
-            args.atoms.write("\t")
-            args.atoms.write(f"{atom_pair[1].get_parent().get_parent().get_id()}")
-            args.atoms.write("\t")
-            args.atoms.write(f"{atom_pair[1].get_parent().get_id()[1]}")
-            args.atoms.write("\t")
-            args.atoms.write(f"{atom_pair[1].get_parent().get_resname()}")
-            args.atoms.write("\t")
-            args.atoms.write(f"{atom_pair[1].get_name()}")
-            args.atoms.write("\n")
+        interactions = search_interactions(neighbor_search, args.radius, level='A')
+        write_atoms(interactions, args.radius, args.atoms)
+
+
+def potential_interactor_atoms(chain: "chain from PDB file") -> "list of atoms that can interact with other atoms":
+    """Returns list of atoms that can interact with other atoms"""
+    atoms = []
+    for residue in chain:
+        for atom in residue:
+            if atom.get_name() not in ["N", "CA", "C", "O", "OXT"] and not atom.get_name().startswith("H"):
+                atoms.append(atom)
+    return atoms
+
+
+def search_interactions(neighbor_search: NeighborSearch, radius: float, level: str) -> "list of interactions":
+    """Search for interactions"""
+    interactions = neighbor_search.search_all(radius, level)
+    interactions = [residue_pair for residue_pair in interactions
+                    if get_chain(residue_pair[0], level) != get_chain(residue_pair[1], level)]
+    return interactions
+
+
+def get_chain(entity: "PDB entity", level: "Level of entity - 'A' for atom or 'R' for residue" = 'R') -> \
+        "entity's chain":
+    if level == 'R':
+        return entity.get_parent()
+    elif level == 'A':
+        return entity.get_parent().get_parent()
+    else:
+        raise AssertionError(f"Level '{level}' is not one of 'A' or 'R'")
+
+
+def write_residues(interactions: "list of residue interactions", outfile: "output file"):
+    """Write residues that have potential interactions to output file"""
+    outfile.write("Chain A\tResidue number A\tResidue name A\tChain B\tResidue number B\tResidue name B\n")
+    for residue_pair in interactions:
+        for i in range(0, 2):
+            residue = residue_pair[i]
+            chain = residue.get_parent()
+            outfile.write(f"{chain.get_id()}")
+            outfile.write("\t")
+            outfile.write(f"{residue.get_id()[1]}")
+            outfile.write("\t")
+            outfile.write(f"{residue.get_resname()}")
+            outfile.write("\t" if i < 1 else "\n")
+
+
+def write_atoms(interactions: "list of residue interactions", outfile: "output file"):
+    """Write atoms that have potential interactions to output file"""
+    outfile.write("Chain A\tResidue number A\tResidue name A\tAtom A\t"
+                  "Chain B\tResidue number B\tResidue name B\tAtom B\n")
+    for atom_pair in interactions:
+        for i in range(0, 2):
+            atom = atom_pair[i]
+            residue = atom.get_parent()
+            chain = residue.get_parent()
+            outfile.write(f"{chain.get_id()}")
+            outfile.write("\t")
+            outfile.write(f"{residue.get_id()[1]}")
+            outfile.write("\t")
+            outfile.write(f"{residue.get_resname()}")
+            outfile.write("\t")
+            outfile.write(f"{atom.get_name()}")
+            outfile.write("\t")
+            outfile.write("\t" if i < 1 else "\n")
 
 
 if __name__ == '__main__':

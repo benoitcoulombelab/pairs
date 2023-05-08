03908b80c966f74e5217c96ac5ad0753600098fc
Christian Poitras <christian.poitras@ircm.qc.ca>
2023-05-08 14:30:18 -0400

    Adds weight parameters to CountMatrix.py for normalization.

--- DeepProteinConnector/CountMatrix.py
+++ DeepProteinConnector/CountMatrix.py
@@ -1,5 +1,13 @@
 import argparse
+import math
 import sys
+
+
+class Mapping:
+    def __init__(self, source: str, converted: str, weight: float):
+        self.source = source
+        self.converted = converted
+        self.weight = weight
 
 
 def main():
@@ -8,12 +16,19 @@ def main():
                         help="Tab delimited text file containing protein id/accession and counts")
     parser.add_argument('-m', '--mapping', type=argparse.FileType('r'), default="mapping.txt",
                         help="Tab delimited text file containing source ids and converted ids  (default: %(default)s)")
-    parser.add_argument('-s', '--source_column', type=int, default='1',
+    parser.add_argument('-s', '--source', type=int, default='1',
                         help="Column index of source ids in conversion file - 1 means first column of file" +
                              "   (default: %(default)s)")
-    parser.add_argument('-c', '--converted_column', type=int, default='2',
+    parser.add_argument('-c', '--converted', type=int, default='2',
                         help="Column index of converted ids in conversion file - 1 means first column of file" +
                              "   (default: %(default)s)")
+    parser.add_argument('-w', '--weight', type=int,
+                        help="Column index of weights used for normalization - 1 means first column of file" +
+                             "   (default: %(default)s)")
+    parser.add_argument('-b', '--base', type=float, default=1.0,
+                        help="Base value for normalization - values are multiplied by "
+                             "'base_weight / log2(sum weight of both proteins)' " +
+                             "  (default: %(default)s)")
     parser.add_argument('-u', '--unique', action='store_true',
                         help="Protein pairs are unique, so mirror the counts in matrix")
     parser.add_argument('-o', '--output', type=argparse.FileType('w'), default=sys.stdout,
@@ -26,9 +41,10 @@ def main():
         if line.startswith('#'):
             continue
         columns = line.rstrip('\r\n').split('\t')
-        source = columns[args.source_column - 1]
-        converted = columns[args.converted_column - 1]
-        mappings[source] = converted
+        source = columns[args.source - 1]
+        converted = columns[args.converted - 1]
+        weight = float(columns[args.weight - 1]) if args.weight else None
+        mappings[source] = Mapping(source, converted, weight)
 
     matrix = {}
     target_set = set()
@@ -38,8 +54,8 @@ def main():
             if line.startswith('#'):
                 continue
             columns = line.rstrip('\r\n').split('\t')
-            bait = mappings[columns[0]]
-            target = mappings[columns[1]]
+            bait = columns[0]
+            target = columns[1]
             target_set.add(target)
             if args.unique:
                 target_set.add(bait)
@@ -57,18 +73,23 @@ def main():
                 matrix[target][bait] = max(matrix[target][bait], count)
 
     bait_list = list(matrix)
-    bait_list.sort()
+    bait_list.sort(key=lambda m: mappings[m].converted)
     target_list = list(target_set)
-    target_list.sort()
+    target_list.sort(key=lambda m: mappings[m].converted)
 
     args.output.write("Bait")
-    for gene in target_list:
-        args.output.write(f"\t{gene}")
+    for target in target_list:
+        target_mapping = mappings[target]
+        args.output.write(f"\t{target_mapping.converted}")
     args.output.write('\n')
     for bait in bait_list:
-        args.output.write(f"{bait}")
+        bait_mapping = mappings[bait]
+        args.output.write(f"{bait_mapping.converted}")
         for target in target_list:
+            target_mapping = mappings[target]
             count = matrix[bait][target] if target in matrix[bait] else None
+            if count and args.weight:
+                count = count * args.base / math.log2(bait_mapping.weight + target_mapping.weight)
             args.output.write(f"\t{count if count is not None else ''}")
         args.output.write('\n')
 

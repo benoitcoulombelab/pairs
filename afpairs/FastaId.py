9d3f32ceb386c28382158aa0f97a3685e48b2d50
Christian Poitras <christian.poitras@ircm.qc.ca>
2023-07-26 12:09:53 -0400

    Adds unit tests for FastaId.py.

--- afpairs/FastaId.py
+++ afpairs/FastaId.py
@@ -1,58 +1,58 @@
 import argparse
-import os
 import re
 import sys
 
+from Bio import SeqIO
 
-def main():
+
+def main(argv: list[str] = None):
     parser = argparse.ArgumentParser(description="Prints id of FASTA file. \n\n"
-                                     "If no option is used or if it fails to use the option, an accession number "
-                                     "from the FASTA file will be printed.\n"
-                                     "Accession is assumed to be the third element, when possible "
-                                     "like P24928 in sp|P24928|RPB1_HUMAN.\n"
-                                     "If there is no third element, the second is used, otherwise the first is used")
+                                                 "If no option is used or if it fails to use the option, an accession "
+                                                 "number from the FASTA file will be printed.\n"
+                                                 "Accession is assumed to be the last element, when possible "
+                                                 "like RPB1_HUMAN in sp|P24928|RPB1_HUMAN.")
     parser.add_argument('infile', nargs='?', type=argparse.FileType('r'), default=sys.stdin,
                         help="FASTA file")
     parser.add_argument('-g', '--gene', action='store_true',
                         help="Use gene name as FASTA id.")
 
-    args = parser.parse_args()
+    args = parser.parse_args(argv)
 
-    for line in args.infile:
-        if line.startswith('>'):
-            sequence_name = line
+    try:
+        sequence_name = next(SeqIO.parse(args.infile, "fasta")).description
+    except StopIteration:
+        raise AssertionError(f"Could not find any sequence in file {args.infile}")
 
-    if not sequence_name:
-        sys.exit(1, "Could not find sequence name starting with '>'")
-
-    sequence_id = fasta_id(sequence_name, args.gene)
+    sequence_id = fasta_id(sequence_name, gene=args.gene)
     if id:
         print(sequence_id)
-        sys.exit(0)
     else:
-        sys.exit(1, "Could not find any id in sequence name")
+        raise AssertionError("Could not find any id in sequence name")
 
 
-def fasta_id(sequence_name, gene=False):
+def fasta_id(sequence_name: str, gene: bool = False):
     """
     Returns sequence accession/id from the sequence name.
 
     When possible, an accession number will be returned.
-    Accession number is assumed to be the third element like P24928 in sp|P24928|RPB1_HUMAN.
-
-    If there is no third element, the second is used, otherwise the first is used.
+    Accession number is assumed to be the last element like RPB1_HUMAN in sp|P24928|RPB1_HUMAN.
 
     :param sequence_name: Sequence name - the complete line of FASTA starting with '>'
     :param gene: If True, will return the gene name if found
     :return: Sequence id from the sequence name
     """
-    patterns = [r"[\w]+\|[\w]+\|([\w]+)", r"[\w]+\|([\w]+)", r"([\w]+)"]
     if gene:
-        patterns.insert(0, r"GN=([\w]+)")
-    for pattern in patterns:
+        pattern = r"GN=([\w]+)"
         re_search = re.search(pattern, sequence_name)
         if re_search:
             return f"{re_search[1]}"
+    sequence_name = sequence_name.split(" ")[0]
+    pattern = r">?\|?([^\|]+)\|?$"
+    re_search = re.search(pattern, sequence_name)
+    if re_search:
+        sequence_name = re_search[1]
+        sequence_name = re.sub(r"\W.*", "", sequence_name)
+        return f"{sequence_name}"
     return None
 
 

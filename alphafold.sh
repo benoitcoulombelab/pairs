b8263ecf25009e3f6924320db6e8956548f6675d
Christian Poitras <christian.poitras@ircm.qc.ca>
2023-08-12 10:35:41 -0400

    Changes alphafold.sh script to support AlphaFold as a Python package.

--- alphafold.sh
+++ alphafold.sh
@@ -5,7 +5,7 @@ set -e
 
 fasta=$1
 output=$2
-step=${3:-all}
+step=${3:-alphafold}
 max_template_date=$(date +"%Y-%m-%d")
 
 echo -e "\n\nRun AlphaFold with step ${step} on fasta ${fasta}\n\n"
@@ -15,43 +15,84 @@ if [[ -n "$CC_CLUSTER" ]]
 then
   module purge
   module load StdEnv/2020
-  module load gcc/9.3.0 cuda/11.7
-  module load apptainer/1.1
+  module load gcc/9.3.0 openmpi/4.0.3 cuda/11.4 cudnn/8.2.0 kalign/2.03 hmmer/3.2.1 openmm-alphafold/7.5.1 \
+              hh-suite/3.3.0 python/3.8
   module load alphafold/2.3.2
 fi
-if [[ -n "$SLURM_TMPDIR" ]]
-then
-  # Set TMPDIR to fast local storage.
-  export TMPDIR="$SLURM_TMPDIR"
-fi
+data_dir="$ALPHAFOLD_SMALL_DATADIR"
 
 ### Check values of some environment variables
 echo SLURM_JOB_ID="$SLURM_JOB_ID"
 echo SLURM_JOB_GPUS="$SLURM_JOB_GPUS"
-echo ALPHAFOLD_DIR="$ALPHAFOLD_DIR"
-echo ALPHAFOLD_DATADIR="$ALPHAFOLD_DATADIR"
 
 ### Check values of variables
 echo "output=${output}"
 echo "max_template_date=${max_template_date}"
+echo "data_dir=${data_dir}"
 
+if [[ -n "$CC_CLUSTER" ]]
+then
+  echo "Create AlphaFold virtual environment in ${SLURM_TMPDIR}"
+  venv="${SLURM_TMPDIR}/venv"
+  virtualenv --no-download "$venv"
+  source "${venv}/bin/activate"
 
-gpu_parameters=("--use_gpu" "--gpu_devices=${SLURM_JOB_GPUS}")
+  # Install alphafold and its dependencies
+  pip install --no-index --upgrade pip
+  pip install --no-index --requirement "${ALPHAFOLD}/alphafold-requirements.txt"
+  pushd "${venv}/bin"
+  git apply "${ALPHAFOLD}/alphafold-${ALPHAFOLD_VERSION}.patch"
+  popd
+fi
+
+step_parameters=("--use_gpu_relax=True" "--use_precomputed_msas=True")
 if [ "${step}" == "prepare" ]
 then
-  gpu_parameters=("--use_gpu=False")
+  step_parameters=("--prepare" "--use_gpu_relax=False" "--use_precomputed_msas=False")
 fi
 
 mkdir -p "${output}"
-"${ALPHAFOLD}/venv/bin/python" "${ALPHAFOLD_DIR}/singularity/run_singularity.py" \
-    --step="$step" \
-    "${gpu_parameters[@]}" \
+
+if [[ -n "$SLURM_TMPDIR" ]]
+then
+  echo "Copy data from ${output} to fast local storage ${SLURM_TMPDIR}"
+  copy_fasta="$fasta"
+  fasta="${SLURM_TMPDIR}/${fasta##*/}"
+  cp "$copy_fasta" "$fasta"
+  copy_output="$output"
+  output="${SLURM_TMPDIR}/${output##*/}"
+  cp -r "${copy_output}" "$output"
+fi
+
+set +e
+echo "Start AlphaFold using run_alphafold.py"
+run_alphafold.py \
+    "${step_parameters[@]}" \
     --fasta_paths="$fasta" \
+    --output_dir="${output}" \
     --max_template_date="$max_template_date" \
-    --data_dir="$ALPHAFOLD_DATADIR" \
-    --model_preset=multimer \
+    --data_dir="$data_dir" \
     --db_preset=reduced_dbs \
-    --output_dir="${output}"
+    --model_preset=multimer \
+    --small_bfd_database_path="${data_dir}/small_bfd/bfd-first_non_consensus_sequences.fasta" \
+    --mgnify_database_path="${data_dir}/mgnify/mgy_clusters_2022_05.fa" \
+    --template_mmcif_dir="${data_dir}/pdb_mmcif/mmcif_files" \
+    --obsolete_pdbs_path="${data_dir}/pdb_mmcif/obsolete.dat" \
+    --pdb_seqres_database_path="${data_dir}/pdb_seqres/pdb_seqres.txt" \
+    --uniprot_database_path="${data_dir}/uniprot/uniprot.fasta" \
+    --uniref90_database_path="${data_dir}/uniref90/uniref90.fasta" \
+    --hhblits_binary_path="${EBROOTHHMINSUITE}/bin/hhblits" \
+    --hhsearch_binary_path="${EBROOTHHMINSUITE}/bin/hhsearch" \
+    --jackhmmer_binary_path="${EBROOTHMMER}/bin/jackhmmer" \
+    --kalign_binary_path="${EBROOTKALIGN}/bin/kalign"
 alphafold_return=$?
 
 echo "INFO: AlphaFold return code is ${alphafold_return}"
+
+if [[ -n "$SLURM_TMPDIR" ]]
+then
+  echo "Copy output from fast local storage ${SLURM_TMPDIR} to ${copy_output}"
+  cp -r "${output}/*" "$copy_output"
+fi
+
+exit "$alphafold_return"

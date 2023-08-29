f02c671d7c992bf4430b37752f2de2e5643dc1de
Christian Poitras <christian.poitras@ircm.qc.ca>
2023-08-29 19:13:34 -0400

    Improves SplitFasta.py and adds unit tests.

--- afpairs/SplitFasta.py
+++ afpairs/SplitFasta.py
@@ -1,8 +1,11 @@
 import argparse
 import os
 import sys
+from typing import TextIO
 
 from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
+
 from afpairs import FastaId
 
 
@@ -13,22 +16,40 @@ def dir_path(string):
         raise NotADirectoryError(string)
 
 
-def main():
+def main(argv: list[str] = None):
     parser = argparse.ArgumentParser(description="Create FASTA files containing one sequence per file.")
     parser.add_argument('input', nargs='?', type=argparse.FileType('r'), default=sys.stdin,
                         help="FASTA file containing baits")
     parser.add_argument('-o', '--output', type=dir_path, default="",
                         help="Directory where to write FASTA files.  (default: current directory)")
 
-    args = parser.parse_args()
+    args = parser.parse_args(argv)
+    split_fasta(fasta=args.input, output_dir=args.output)
 
-    sequences = parse_fasta(args.input)
+
+def split_fasta(fasta: TextIO, output_dir: str):
+    """
+    Splits FASTA file into one file per sequence.
+
+    The name of the output file will be the "{id}.fasta where" "{id}" is the id returned by
+    :func:`~afpairs.FastaId.fasta_id`
+
+    :param fasta: FASTA file containing multiple sequences
+    :param output_dir: directory where to write files
+    """
+    sequences = parse_fasta(fasta)
 
     for sequence_id in sequences:
-        SeqIO.write(sequences[sequence_id], os.path.join(args.output, f"{sequence_id}.fasta"), "fasta")
+        SeqIO.write(sequences[sequence_id], os.path.join(output_dir, f"{sequence_id}.fasta"), "fasta")
 
 
-def parse_fasta(fasta):
+def parse_fasta(fasta: TextIO) -> list[SeqRecord]:
+    """
+    Parses FASTA file.
+
+    :param fasta: FASTA file
+    :return: list of sequences from FASTA file
+    """
     sequences = {}
     for record in SeqIO.parse(fasta, "fasta"):
         seq_id = FastaId.fasta_id(record.description)

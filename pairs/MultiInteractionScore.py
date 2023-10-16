8579eb7b63bbbaaf79c5337b36372be123a36535
Christian Poitras <christian.poitras@ircm.qc.ca>
2023-10-16 18:01:50 -0400

    Adds stats parameter to MultiInteractionScore.py.
    This allows to show confidence of AlphaFold in its predictions.

--- pairs/MultiInteractionScore.py
+++ pairs/MultiInteractionScore.py
@@ -2,12 +2,26 @@ import argparse
 import os
 import re
 import sys
+import glob
+import statistics
+import json
 from typing import TextIO
 
 import smokesignal
 import tqdm
 
 from pairs import InteractionScore
+
+
+class AlphafoldStatistics:
+    def __init__(self, directory: str, ranked0_score: float, ranked0_confidence: float, average_confidence: float,
+                 unrelaxed_average_score: float, unrelaxed_score_standard_deviation: float):
+        self.directory = directory
+        self.ranked0_score = ranked0_score
+        self.ranked0_confidence = ranked0_confidence
+        self.average_confidence = average_confidence
+        self.unrelaxed_average_score = unrelaxed_average_score
+        self.unrelaxed_score_standard_deviation = unrelaxed_score_standard_deviation
 
 
 def file_path(string: str):
@@ -22,10 +36,12 @@ def main(argv: list[str] = None):
     parser.add_argument('inputs', nargs='+', type=file_path,
                         help="PDB files for which to compute PPI score")
     parser.add_argument('-o', '--output', type=argparse.FileType('w'), default=sys.stdout,
-                        help="Tab delimited output file containing counts")
+                        help="Tab delimited output file containing scores")
     parser.add_argument('-n', '--name', default=r"([\w-]+)__([\w-]+)",
                         help="Regular expression to obtain protein/gene names based on PDB filename "
                              " (default: %(default)s)")
+    parser.add_argument('-s', '--stats', action="store_true", default=False,
+                        help="Compute confidence and unrelaxed score statistics")
     parser.add_argument('-a', '--first', default="A",
                         help="Chains of first protein separated by ','  (default: %(default)s)")
     parser.add_argument('-b', '--second', default="B",
@@ -57,6 +73,7 @@ def main(argv: list[str] = None):
     smokesignal.on(InteractionScore.MISSING_CHAIN_EVENT, InteractionScore.warn_missing_chain, max_calls=1)
 
     multi_interaction_score(input_files=args.inputs, output_file=args.output, name=args.name,
+                            stats=args.stats,
                             radius=args.radius, weight=args.weight, count=args.count, progress=args.progress,
                             first_chains=args.first, second_chains=args.second,
                             partial=args.partial,
@@ -65,6 +82,7 @@ def main(argv: list[str] = None):
 
 
 def multi_interaction_score(input_files: list[str], output_file: TextIO = sys.stdout, name: str = r"([\w-]+)__([\w-]+)",
+                            stats: bool = False,
                             radius: float = 6, weight: bool = False, count: bool = False, progress: bool = False,
                             first_chains: list[str] = ["A"], second_chains: list[str] = ["B"],
                             partial: bool = False,
@@ -74,6 +92,7 @@ def multi_interaction_score(input_files: list[str], output_file: TextIO = sys.st
 
     :param input_files: PDB files
     :param output_file: output file
+    :param stats: compute confidence and unrelaxed score statistics
     :param name: regular expression to obtain protein/gene names based on PDB filename
     :param radius: maximal distance between two residues' atoms to consider that the two residues interact
     :param weight: if True, normalize score by proteins' weight
@@ -89,7 +108,12 @@ def multi_interaction_score(input_files: list[str], output_file: TextIO = sys.st
     mappings = {}
     if mapping_file:
         mappings = parse_mapping(mapping_file, source_column, converted_column)
-    output_file.write("Bait\tTarget\tScore\n")
+    output_file.write("Bait\tTarget\t")
+    if stats:
+        output_file.write("Ranked_0 score\tRanked_0 confidence\tAverage confidence\t"
+                          "Unrelaxed average score\t Unrelaxed score standard deviation\n")
+    else:
+        output_file.write("Score\n")
     for input_file in (tqdm.tqdm(input_files) if progress else input_files):
         re_match = re.search(name, input_file)
         if not re_match:
@@ -97,11 +121,69 @@ def multi_interaction_score(input_files: list[str], output_file: TextIO = sys.st
         bait, target = re_match.group(1, 2)
         bait = mappings[bait] if bait in mappings else bait
         target = mappings[target] if target in mappings else target
-        with open(input_file, 'r') as input_in:
-            score = InteractionScore.interaction_score(
-                pdb=input_in, radius=radius, weight=weight, count=count,
-                first_chains=first_chains, second_chains=second_chains, partial=partial)
-        output_file.write(f"{bait}\t{target}\t{score}\n")
+        output_file.write(f"{bait}\t{target}\t")
+        if stats:
+            a_s = alphafold_statistics(directory=os.path.dirname(input_file), radius=radius, weight=weight, count=count,
+                                       first_chains=first_chains, second_chains=second_chains, partial=partial)
+            output_file.write(f"{a_s.ranked0_score}\t{a_s.ranked0_confidence}\t{a_s.average_confidence}\t"
+                              f"{a_s.unrelaxed_average_score}\t{a_s.unrelaxed_score_standard_deviation}\n")
+        else:
+            with open(input_file, 'r') as input_in:
+                score = InteractionScore.interaction_score(
+                    pdb=input_in, radius=radius, weight=weight, count=count,
+                    first_chains=first_chains, second_chains=second_chains, partial=partial)
+            output_file.write(f"{score}\n")
+
+
+def alphafold_statistics(directory: str, radius: float = 6, weight: bool = False, count: bool = False,
+                         first_chains: list[str] = ["A"], second_chains: list[str] = ["B"],
+                         partial: bool = False) -> AlphafoldStatistics:
+    """
+    Computes statistics on AlphaFold's output.
+
+    :param directory: AlphaFold's output
+    :param radius: maximal distance between two residues' atoms to consider that the two residues interact
+    :param weight: if True, normalize score by proteins' weight
+    :param count: if True, score is the number of residue pairs below radius
+    :param first_chains: chains of the first protein
+    :param second_chains: chains of the second protein
+    :param partial: do not warn if all chains in PDB are not used for computing score
+    :return: statistics on AlphaFold's output
+    """
+    ranked0_file = os.path.join(directory, "ranked_0.pdb")
+    if not os.path.isfile(ranked0_file):
+        raise AssertionError(f"ranked_0.pdb does not exists in directory {directory}")
+    with open(ranked0_file, 'r') as input_in:
+        ranked0_score = InteractionScore.interaction_score(
+            pdb=input_in, radius=radius, weight=weight, count=count,
+            first_chains=first_chains, second_chains=second_chains, partial=partial)
+    ranking_file = os.path.join(directory, "ranking_debug.json")
+    ranked0_confidence = None
+    average_confidence = None
+    if os.path.isfile(ranking_file):
+        with open(ranking_file, 'r') as input_in:
+            rankings = json.load(input_in)
+        ranked0_model = rankings["order"][0]
+        ranked0_confidence = rankings["iptm+ptm"][ranked0_model]
+        average_confidence = statistics.mean(rankings["iptm+ptm"].values())
+    unrelaxed_files = glob.glob(os.path.join(directory, "unrelaxed_*.pdb"))
+    unrelaxed_average_score = None
+    unrelaxed_score_standard_deviation = None
+    if unrelaxed_files:
+        unrelaxed_scores = []
+        for unrelaxed_file in unrelaxed_files:
+            with open(unrelaxed_file, 'r') as input_in:
+                unrelaxed_scores.append(InteractionScore.interaction_score(
+                    pdb=input_in, radius=radius, weight=weight, count=count,
+                    first_chains=first_chains, second_chains=second_chains, partial=partial))
+        unrelaxed_average_score = statistics.mean(unrelaxed_scores)
+        unrelaxed_score_standard_deviation = statistics.pstdev(unrelaxed_scores)
+    return AlphafoldStatistics(
+        directory,
+        ranked0_score=ranked0_score, ranked0_confidence=ranked0_confidence,
+        average_confidence=average_confidence,
+        unrelaxed_average_score=unrelaxed_average_score,
+        unrelaxed_score_standard_deviation=unrelaxed_score_standard_deviation)
 
 
 def parse_mapping(mapping_file: TextIO, source_column: int = 0, converted_column: int = 1) \
--- tests/pairs/test_MultiInteractionScore.py
+++ tests/pairs/test_MultiInteractionScore.py
@@ -1,5 +1,6 @@
 import os.path
 import sys
+import shutil
 from io import TextIOWrapper
 from pathlib import Path
 from unittest.mock import MagicMock, ANY, patch
@@ -14,12 +15,14 @@ from pairs import InteractionScore, MultiInteractionScore
 def mock_testclass():
     _file_path = MultiInteractionScore.file_path
     _multi_interaction_score = MultiInteractionScore.multi_interaction_score
+    _alphafold_statistics = MultiInteractionScore.alphafold_statistics
     _parse_mapping = MultiInteractionScore.parse_mapping
     _interaction_score = InteractionScore.interaction_score
     _smokesignal_on = smokesignal.on
     yield
     MultiInteractionScore.file_path = _file_path
     MultiInteractionScore.multi_interaction_score = _multi_interaction_score
+    MultiInteractionScore.alphafold_statistics = _alphafold_statistics
     MultiInteractionScore.parse_mapping = _parse_mapping
     InteractionScore.interaction_score = _interaction_score
     smokesignal.on = _smokesignal_on
@@ -32,8 +35,9 @@ def test_main(testdir, mock_testclass):
     smokesignal.on = MagicMock()
     MultiInteractionScore.main(pdbs)
     MultiInteractionScore.multi_interaction_score.assert_called_once_with(
-        input_files=pdbs, output_file=ANY, name=r"([\w-]+)__([\w-]+)", radius=6.0, weight=False, count=False,
-        progress=False,
+        input_files=pdbs, output_file=ANY, name=r"([\w-]+)__([\w-]+)",
+        stats=False,
+        radius=6.0, weight=False, count=False, progress=False,
         first_chains=["A"], second_chains=["B"], partial=False,
         mapping_file=None, source_column=0, converted_column=1)
     output_file = MultiInteractionScore.multi_interaction_score.call_args.kwargs["output_file"]
@@ -52,11 +56,12 @@ def test_main_parameters(testdir, mock_testclass):
     MultiInteractionScore.multi_interaction_score = MagicMock()
     smokesignal.on = MagicMock()
     MultiInteractionScore.main(
-        ["-o", output_file, "-n", r"(\w+)____(\w+)", "-a", "A,B", "-b", "C,D", "-r", "9", "-w", "-c", "-p", "-P",
+        ["-o", output_file, "-n", r"(\w+)____(\w+)", "-s", "-a", "A,B", "-b", "C,D", "-r", "9", "-w", "-c", "-p", "-P",
          "-M", mapping_file, "-S", "2", "-C", "3"] + pdbs)
     MultiInteractionScore.multi_interaction_score.assert_called_once_with(
-        input_files=pdbs, output_file=ANY, name=r"(\w+)____(\w+)", radius=9.0, weight=True, count=True,
-        progress=True,
+        input_files=pdbs, output_file=ANY, name=r"(\w+)____(\w+)",
+        stats=True,
+        radius=9.0, weight=True, count=True, progress=True,
         first_chains=["A", "B"], second_chains=["C", "D"], partial=True,
         mapping_file=ANY, source_column=1, converted_column=2)
     mapping_in = MultiInteractionScore.multi_interaction_score.call_args.kwargs["mapping_file"]
@@ -78,12 +83,13 @@ def test_main_long_parameters(testdir, mock_testclass):
     MultiInteractionScore.multi_interaction_score = MagicMock()
     smokesignal.on = MagicMock()
     MultiInteractionScore.main(
-        ["--output", output_file, "--name", r"(\w+)____(\w+)", "--first", "A,B", "--second", "C,D", "--radius", "9",
-         "--weight", "--count", "--progress", "--partial",
+        ["--output", output_file, "--name", r"(\w+)____(\w+)", "--stats", "--first", "A,B", "--second", "C,D",
+         "--radius", "9", "--weight", "--count", "--progress", "--partial",
          "--mapping", mapping_file, "--source_column", "2", "--converted_column", "3"] + pdbs)
     MultiInteractionScore.multi_interaction_score.assert_called_once_with(
-        input_files=pdbs, output_file=ANY, name=r"(\w+)____(\w+)", radius=9.0, weight=True, count=True,
-        progress=True,
+        input_files=pdbs, output_file=ANY, name=r"(\w+)____(\w+)",
+        stats=True,
+        radius=9.0, weight=True, count=True, progress=True,
         first_chains=["A", "B"], second_chains=["C", "D"], partial=True,
         mapping_file=ANY, source_column=1, converted_column=2)
     mapping_in = MultiInteractionScore.multi_interaction_score.call_args.kwargs["mapping_file"]
@@ -233,6 +239,108 @@ def test_multi_interaction_score_progress(testdir, mock_testclass):
         assert output_in.readline() == "POLR2A\tPOLR2B\t2.3\n"
         assert output_in.readline() == "POLR2C\tPOLR2J-I\t4.5\n"
         assert output_in.readline() == "POLR2D-E\tPOLR2G\t8.2\n"
+
+
+def test_multi_interaction_score_statistics(testdir, mock_testclass):
+    directories = ["POLR2A__POLR2B", "POLR2C__POLR2J", "POLR2D__POLR2G"]
+    ranked0_files = [os.path.join(directory, "ranked_0.pdb") for directory in directories]
+    [testdir.mkdir(directory) for directory in directories]
+    [open(f, 'w').close() for f in ranked0_files]
+    as1 = MultiInteractionScore.AlphafoldStatistics(directories[0], 8.2, 0.8, 0.7, 7.3, 1.1)
+    as2 = MultiInteractionScore.AlphafoldStatistics(directories[1], 6.5, 0.5, 0.45, 3.5, 2.3)
+    as3 = MultiInteractionScore.AlphafoldStatistics(directories[2], 10.3, 0.3, 0.2, 0.9, 3.4)
+    MultiInteractionScore.alphafold_statistics = MagicMock(side_effect=[as1, as2, as3])
+    output_file = "output.txt"
+    with open(output_file, 'w') as output_out:
+        MultiInteractionScore.multi_interaction_score(ranked0_files, output_file=output_out, stats=True)
+    for directory in directories:
+        MultiInteractionScore.alphafold_statistics.assert_any_call(
+            directory=directory, radius=6.0, weight=False, count=False,
+            first_chains=["A"], second_chains=["B"], partial=False)
+    assert os.path.isfile(output_file)
+    with open(output_file, 'r') as output_in:
+        assert output_in.readline() == "Bait\tTarget\t" \
+                                       "Ranked_0 score\tRanked_0 confidence\tAverage confidence\t" \
+                                       "Unrelaxed average score\t Unrelaxed score standard deviation\n"
+        assert output_in.readline() == "POLR2A\tPOLR2B\t8.2\t0.8\t0.7\t7.3\t1.1\n"
+        assert output_in.readline() == "POLR2C\tPOLR2J\t6.5\t0.5\t0.45\t3.5\t2.3\n"
+        assert output_in.readline() == "POLR2D\tPOLR2G\t10.3\t0.3\t0.2\t0.9\t3.4\n"
+
+
+def test_multi_interaction_score_statistics_parameters(testdir, mock_testclass):
+    directories = ["POLR2A__POLR2B", "POLR2C__POLR2J", "POLR2D__POLR2G"]
+    ranked0_files = [os.path.join(directory, "ranked_0.pdb") for directory in directories]
+    [testdir.mkdir(directory) for directory in directories]
+    [open(f, 'w').close() for f in ranked0_files]
+    as1 = MultiInteractionScore.AlphafoldStatistics(directories[0], 8.2, 0.8, 0.7, 7.3, 1.1)
+    as2 = MultiInteractionScore.AlphafoldStatistics(directories[1], 6.5, 0.5, 0.45, 3.5, 2.3)
+    as3 = MultiInteractionScore.AlphafoldStatistics(directories[2], 10.3, 0.3, 0.2, 0.9, 3.4)
+    MultiInteractionScore.alphafold_statistics = MagicMock(side_effect=[as1, as2, as3])
+    output_file = "output.txt"
+    with open(output_file, 'w') as output_out:
+        MultiInteractionScore.multi_interaction_score(
+            ranked0_files, output_file=output_out, stats=True, radius=9.0, weight=True, count=True,
+            first_chains=["A", "B"], second_chains=["C", "D"], partial=True)
+    for directory in directories:
+        MultiInteractionScore.alphafold_statistics.assert_any_call(
+            directory=directory, radius=9.0, weight=True, count=True,
+            first_chains=["A", "B"], second_chains=["C", "D"], partial=True)
+    assert os.path.isfile(output_file)
+    with open(output_file, 'r') as output_in:
+        assert output_in.readline() == "Bait\tTarget\t" \
+                                       "Ranked_0 score\tRanked_0 confidence\tAverage confidence\t" \
+                                       "Unrelaxed average score\t Unrelaxed score standard deviation\n"
+        assert output_in.readline() == "POLR2A\tPOLR2B\t8.2\t0.8\t0.7\t7.3\t1.1\n"
+        assert output_in.readline() == "POLR2C\tPOLR2J\t6.5\t0.5\t0.45\t3.5\t2.3\n"
+        assert output_in.readline() == "POLR2D\tPOLR2G\t10.3\t0.3\t0.2\t0.9\t3.4\n"
+
+
+def test_alphafold_statistics(testdir, mock_testclass):
+    directory = testdir.mkdir("FAB_5_3__HVM13_MOUSE")
+    ranked0_file = Path(__file__).parent.joinpath("FAB_5_3__HVM13_MOUSE_ranked_0.pdb")
+    shutil.copy(ranked0_file, os.path.join(directory, "ranked_0.pdb"))
+    rankings_file = Path(__file__).parent.joinpath("ranking_debug.json")
+    shutil.copy(rankings_file, directory)
+    unrelaxed_files = ["unrelaxed_model_1_multimer_v3_pred_0.pdb", "unrelaxed_model_1_multimer_v3_pred_1.pdb",
+                       "unrelaxed_model_2_multimer_v3_pred_0.pdb"]
+    [open(os.path.join(directory, f), 'w').close() for f in unrelaxed_files]
+    InteractionScore.interaction_score = MagicMock(side_effect=[8.2, 2.3, 4.5, 8.2])
+    alphafold_statistics = MultiInteractionScore.alphafold_statistics(directory)
+    InteractionScore.interaction_score.assert_any_call(
+        pdb=ANY, radius=6.0, weight=False, count=False, first_chains=["A"], second_chains=["B"], partial=False)
+    assert InteractionScore.interaction_score.call_args_list[0].kwargs["pdb"].name\
+           == os.path.join(directory, "ranked_0.pdb")
+    unrelaxed_files_called = [InteractionScore.interaction_score.call_args_list[i].kwargs["pdb"].name
+                              for i in range(1, len(unrelaxed_files) + 1)]
+    for unrelaxed_file in unrelaxed_files:
+        assert os.path.join(directory, unrelaxed_file) in unrelaxed_files_called
+    assert alphafold_statistics.directory == directory
+    assert alphafold_statistics.ranked0_score == 8.2
+    assert abs(alphafold_statistics.ranked0_confidence - 0.4154) < 0.0001, \
+        f"{alphafold_statistics.ranked0_confidence}"
+    assert abs(alphafold_statistics.average_confidence - 0.3146) < 0.0001, \
+        f"{alphafold_statistics.average_confidence}"
+    assert abs(alphafold_statistics.unrelaxed_average_score - 5.0) < 0.01, \
+        f"{alphafold_statistics.unrelaxed_average_score}"
+    assert abs(alphafold_statistics.unrelaxed_score_standard_deviation - 2.43) < 0.01, \
+        f"{alphafold_statistics.unrelaxed_score_standard_deviation}"
+
+
+def test_alphafold_statistics_ranked0_only(testdir, mock_testclass):
+    directory = testdir.mkdir("FAB_5_3__HVM13_MOUSE")
+    ranked0_file = Path(__file__).parent.joinpath("FAB_5_3__HVM13_MOUSE_ranked_0.pdb")
+    shutil.copy(ranked0_file, os.path.join(directory, "ranked_0.pdb"))
+    InteractionScore.interaction_score = MagicMock(return_value=8.2)
+    alphafold_statistics = MultiInteractionScore.alphafold_statistics(directory)
+    InteractionScore.interaction_score.assert_called_once_with(
+        pdb=ANY, radius=6.0, weight=False, count=False, first_chains=["A"], second_chains=["B"], partial=False)
+    assert InteractionScore.interaction_score.call_args.kwargs["pdb"].name == os.path.join(directory, "ranked_0.pdb")
+    assert alphafold_statistics.directory == directory
+    assert alphafold_statistics.ranked0_score == 8.2
+    assert alphafold_statistics.ranked0_confidence is None
+    assert alphafold_statistics.average_confidence is None
+    assert alphafold_statistics.unrelaxed_average_score is None
+    assert alphafold_statistics.unrelaxed_score_standard_deviation is None
 
 
 def test_parse_mapping(testdir, mock_testclass):

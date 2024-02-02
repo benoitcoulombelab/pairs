6baca3e3ae924d8d0dafa7f6cf9443dc23b4ef55
Christian Poitras <christian.poitras@ircm.qc.ca>
2024-02-02 11:20:43 -0500

    Version is now 0.3.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pairs",
-    version="0.2",
+    version="0.3",
     packages=find_packages(),
     author="Christian Poitras",
     author_email="christian.poitras@ircm.qc.ca",

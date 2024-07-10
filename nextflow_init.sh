47f2bd89879f97e583e0239a0322d86b9e45153c
Christian Poitras <christian.poitras@ircm.qc.ca>
2024-07-10 13:46:39 -0400

    Adds nextflow_init.sh script to initialize Netflow on Compute Canada servers.

---
+++ nextflow_init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# exit when any command fails
+set -e
+
+# load required modules
+if [[ -n "$CC_CLUSTER" ]]
+then
+  module purge
+  module load StdEnv/2020
+  module load nextflow/22.10.6
+fi
+if [[ "beluga" == "$CC_CLUSTER" ]]
+then
+  ulimit -v 40000000
+fi
+
+export NXF_OPTS="-Xms500M -Xmx8000M"
